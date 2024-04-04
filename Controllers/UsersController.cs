4f614791e6f16144d8b882e07d488d75012d7370
beth-codes <70582711+beth-codes@users.noreply.github.com>
2024-04-04 10:26:26 +0100

    Update UsersController.cs

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -65,6 +65,7 @@ public class UsersController : ControllerBase
                 worksheet.Cells[newRow, 3].Value = employee.JobTitle;
                 worksheet.Cells[newRow, 4].Value = employee.Phone;
                 worksheet.Cells[newRow, 5].Value = employee.Email;
+                 worksheet.Cells[newRow, 6].Value = employee.Id;
                 package.Save();
             }
             return Ok(employee);
